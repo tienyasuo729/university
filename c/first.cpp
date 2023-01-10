e7562cd1765229686a0f73695cd8de45dfd0e063
tienyasuo729 <tienngqueson@gmail.com>
2023-01-10 11:35:05 +0700

    codegym

--- c/first.cpp
+++ c/first.cpp
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <conio.h>
 int main (){
-	char c = 0101;
-	int i = 1;
+	int m = 1;
 	long l = 1000;
-	float f = 0.5f;
-	double d = 12.089;
-	printf("variable c : at addr: %u, value : %c, size : %d \n", &c ,c , sizeof(c));		
-	printf("variable i : at addr: %u, value : %i, size : %d \n", &i ,i , sizeof(i));
+	float y = 0.5f;
+	short s = 32;
+	printf("variable m : at addr: %u, value : %m, size : %d \n", &m ,m , sizeof(m));
 	printf("variable l : at addr: %u, value : %ld, size : %d \n", &l ,l , sizeof(l));
-	printf("variable f : at addr: %u, value : %f, size : %d \n", &f ,f , sizeof(f));
-	printf("variable d : at addr: %u, value : %lf, size : %d \n", &d ,d , sizeof(d));
-	getch();
+	printf("variable y : at addr: %u, value : %y, size : %d \n", &y ,y , sizeof(y));
+	printf("variable s : at addr: %u, value : %s, size : %d \n", &s ,s , sizeof(s));
+	getchar();
+	return 0;
 }
