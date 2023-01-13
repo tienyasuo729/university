7c8981e1b69b4f4e660aebe64702162363505111
tienyasuo729 <tienngqueson@gmail.com>
2023-01-13 10:01:36 +0700

    codegym

--- c/first.cpp
+++ c/first.cpp
@@ -5,10 +5,10 @@ int main (){
 	long l = 1000;
 	float y = 0.5f;
 	short s = 32;
-	printf("variable m : at addr: %u, value : %m, size : %d \n", &m ,m , sizeof(m));
-	printf("variable l : at addr: %u, value : %ld, size : %d \n", &l ,l , sizeof(l));
+	printf("variable m : at addr: %u, value : %d, size : %d \n", &m ,m , sizeof(m));
+	printf("variable l : at addr: %u, value : %d, size : %d \n", &l ,l , sizeof(l));
 	printf("variable y : at addr: %u, value : %y, size : %d \n", &y ,y , sizeof(y));
-	printf("variable s : at addr: %u, value : %s, size : %d \n", &s ,s , sizeof(s));
+	printf("variable s : at addr: %u, value : %d, size : %d \n", &s ,s , sizeof(s));
 	getchar();
 	return 0;
 }
