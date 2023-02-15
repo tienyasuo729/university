4bcb9f736f3616ed407d1ab6ddc072fee445b36e
tienyasuo729 <tienngqueson@gmail.com>
2023-02-15 11:59:31 +0700

    codegym

--- c/Kiem tra choose.cpp
+++ c/Kiem tra choose.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
-#include<stdlib.h.h>
+#include<stdlib.h>
 
-int choose(int numberOfChoose){
+int checkChoose(int numberOfChoose){
 	int choose = 0;
 	int check = 1;
 	do{
@@ -9,10 +9,26 @@ int choose(int numberOfChoose){
 			scanf("%d", &choose);
 			if(choose >= numberOfChoose || choose <= 0){
 				check = 0;
+			}else{
+				printf("\nBan da nhap sai, xin nhap lai: ");
 			}
-		}catch(int e){
-			printf("\nBan da nhap sai, xin nhap lai: ");
+		}catch(char e){
+			printf("\naaaaaaaaaa ");
 		}
-	}while(check = 1);
+	}while(check == 1);
 	return choose;
+}
+
+int main() {
+  int number;
+  int b;
+  printf("Enter an integer: ");
+  int result = scanf("%d", &number);
+  printf("%d\n",scanf("%d", &b));
+  if (result != 1) {
+    printf("Invalid input. You did not enter an integer.\n");
+  } else {
+    printf("You entered: %d\n", number);
+  }
+  return 0;
 }
