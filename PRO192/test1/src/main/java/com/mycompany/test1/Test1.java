47a6547029e1442b0c20ca7d224059f4691dabd5
tienyasuo729 <tienngqueson@gmail.com>
2023-05-08 09:36:29 +0700

    cdode

---
+++ PRO192/test1/src/main/java/com/mycompany/test1/Test1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.test1;
+
+/**
+ *
+ * @author admin
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        
+    }
+}
