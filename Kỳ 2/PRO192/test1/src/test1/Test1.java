97b0b3be3ed46827376e30e049c47a8f03991970
tienyasuo729 <tienngqueson@gmail.com>
2023-05-15 11:43:08 +0700

    code

---
+++ Kỳ 2/PRO192/test1/src/test1/Test1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package test1;
+
+public class Test1{
+ 
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
